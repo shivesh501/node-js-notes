03d19f2e1f8f81e59a89dda3d581aeb5396bfcc8
Shivesh K <shivkrish501@gmail.com>
2025-08-14 03:25:34 +0530

    delete operation in files using promises

--- tut2/index.js
+++ tut2/index.js
@@ -60,6 +60,14 @@ const fileOps = async () =>{
     try{
         const data = await fsPromises.readFile(path.join(__dirname,'files','starter.txt'),'utf8');
         console.log(data);
+        
+        await fsPromises.unlink(path.join(__dirname,'files','promiseComplete.txt'))//unlink is used to delete a file
+        //using promises to write, append and rename a preexisting file and logging its data
+        await fsPromises.writeFile(path.join(__dirname,'files','promiseWrite.txt'), data);
+        await fsPromises.appendFile(path.join(__dirname,'files','promiseWrite.txt'),'\n\nNice to meet you');
+        await fsPromises.rename(path.join(__dirname,'files','promiseWrite.txt'),path.join(__dirname,'files','promiseComplete.txt'));
+        const newData = await fsPromises.readFile(path.join(__dirname,'files','promiseComplete.txt'),'utf8');
+        console.log(newData);
     }
     catch(err)
     {
