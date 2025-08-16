ebe05374f2e6f6c429be25b90486938c0a4a3da0
Shivesh K <shivkrish501@gmail.com>
2025-08-16 23:48:09 +0530

    node package managers (used nodemon)

---
+++ tut3/index.js
@@ -0,0 +1,15 @@
+/* NPM MODULES */
+
+console.log("testing inside our index.js !")
+
+/* 
+npm i nodemon -g
+-g ----> installs the package globally
+
+nodemon is a utility that monitors for changes in files within a Node.js application. It automatically restarts your application when it detects any changes, so you don’t have to manually stop and restart your server during development. It saves a lot of time, especially when working on larger projects!
+
+For example, if you're building an API with Node.js and making changes to your code, nodemon will automatically restart your server every time you save a file, keeping everything in sync.
+
+nodemone default looks for index.js , use command in the terminal ---> nodemon
+if you want to use a different file , use nodemon followed by filename (eg. server.js) ---> nodemon server
+*/
